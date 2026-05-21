1f6c2a746b2acb97a71a1d5e0f8c4022ec1eb6eb
kmserviceplusitai-droid <kmserviceplus.itai@gmail.com>
2026-05-21 17:39:45 +0700

    update system

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
 const cors = require('cors');
+const { Pool } = require('pg'); // FIXED: Added pg Pool import
 
 const app = express();
 app.use(cors());
